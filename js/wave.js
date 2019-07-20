f17af8ebfa5d9f2ef1dcccacc92afe2ba1a86b70
Dzung Nguyen <dzungng@seas.upenn.edu>
2019-07-19 22:14:48 -0700

    Added bird moving bg

---
+++ js/wave.js
@@ -0,0 +1,18 @@
+// VANTA.WAVES({
+//   el: '#my-wave',
+//   color: 0x7d7376,
+//   waveHeight: 20,
+//   shininess: 50,
+//   waveSpeed: 1,
+//   zoom: 0.75
+// })
+
+VANTA.BIRDS({
+  el: "#my-wave",
+  backgroundColor: 0xeceff2,
+  color1: 0x84809b,
+  color2: 0xb99b9,
+  birdSize: 1.20,
+  speedLimit: 1.00,
+  quantity: 4.00
+})
